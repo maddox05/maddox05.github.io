b69993072f3d1bafafe5b754d24a495db1562848
Light Andy <andy.197197@yahoo.com>
2023-12-09 00:14:43 +0200

    Added files in explorer and footer elements
    Added `home.js`, `about.html`, `github.md`, `projects.py`, `contact.yml` in the EXPLORER tab. In the footer, I've added the branch "main" (if clicked it redirects on this GitHub repository), the warnings and errors and the bell on the right.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,17 @@
 function rotateArrow() {
   const arrowDown = document.getElementById("arrowDown");
+  const files = document.getElementById("files");
   if (arrowDown.classList.contains("right")) {
     arrowDown.style.transform = "rotate(0deg)";
     arrowDown.classList.toggle("right");
+    files.style.display = "block";
   } else {
     arrowDown.style.transform = "rotate(-90deg)";
     arrowDown.classList.toggle("right");
+    files.style.display = "none";
   }
+}
+
+function goTo(link) {
+  window.location.href = link;
 }
