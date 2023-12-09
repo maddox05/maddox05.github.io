aad098ac0f08442c32d1ac8e4a9ddaf17c1a74d4
Light Andy <andy.197197@yahoo.com>
2023-12-09 16:43:59 +0200

    Functionality to buttons
    All the buttons in the sidebar work, all of the files in the EXPLORER also work. And the tabs still work. So now all the buttons should be clickable and working good.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 function goTo(link) {
-  window.location.href = link;
+  window.open(link, "_blank");
 }
 
 function rotateArrow() {
@@ -10,4 +10,37 @@ function rotateArrow() {
   arrowDown.style.transform = isRight ? "rotate(0deg)" : "rotate(-90deg)";
   arrowDown.classList.toggle("right");
   files.style.display = isRight ? "block" : "none";
+}
+
+function changeSource(page) {
+  const iframes = document.getElementsByTagName("iframe");
+  const tabs = document.getElementById("tabs").children;
+
+  Array.from(iframes).forEach((iframe) => {
+    iframe.src = page;
+  });
+
+  const name =
+    page.split(".html")[0].charAt(0).toUpperCase() +
+    page.split(".html")[0].slice(1);
+
+  const tabList = ["home", "about", "github", "projects", "contact", "404"];
+
+  Array.from(tabs).forEach((tab) => {
+    if (tab.id === `tab${name}`) {
+      tab.classList.add("selected");
+    } else {
+      tab.classList.remove("selected");
+    }
+  });
+
+  const pageButtons = document.getElementsByClassName("page-button");
+
+  Array.from(pageButtons).forEach((button) => {
+    if (button.id === `page${name}`) {
+      button.classList.add("selected");
+    } else {
+      button.classList.remove("selected");
+    }
+  });
 }
