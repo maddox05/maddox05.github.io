7247fa9932d98befbd3ea26eadd2a0f32e54468c
Light Andy <andy.197197@yahoo.com>
2023-12-19 23:18:44 +0200

    Update main.js
    Added repositories and followers function for GitHub page.

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,3 +44,15 @@ function changeSource(page) {
     }
   });
 }
+
+fetch("https://api.github.com/users/maddox05/repos")
+  .then((response) => response.json())
+  .then((data) => {
+    document.getElementById("repos").innerText = `${data.length} repositories`;
+  });
+
+fetch("https://api.github.com/users/maddox05/followers")
+  .then((response) => response.json())
+  .then((data) => {
+    document.getElementById("followers").innerText = `${data.length} followers`;
+  });
