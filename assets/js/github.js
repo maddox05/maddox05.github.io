af92b896c5cb46f7cbe991212b24259a26af384f
maddox05 <maddoxschmidlkofer@gmail.com>
2023-12-21 13:19:54 -0500

    spacing

--- assets/js/github.js
+++ assets/js/github.js
@@ -1,11 +1,11 @@
 fetch("https://api.github.com/users/maddox05/repos")
   .then((response) => response.json())
-  .then((data) => {
+    .then((data) => {
     document.getElementById("repos").innerText = `${data.length} repositories`;
   });
 
 fetch("https://api.github.com/users/maddox05/followers")
   .then((response) => response.json())
-  .then((data) => {
+    .then((data) => {
     document.getElementById("followers").innerText = `${data.length} followers`;
   });
